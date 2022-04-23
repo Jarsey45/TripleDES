0335867ab04856e5e040fc6b57af76ecd85907c3
Jarsey45 <bartek.yt01@gmail.com>
2022-04-23 17:31:39 +0200

    #04fix

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-// var coder cipher.Coder;
+
 func main() {
 	keys := [3]string{
 		"123456789ABCDEF1",
@@ -13,12 +13,13 @@ func main() {
 		"292CC794C82AC144",
 	}
 
-	plainText := "8bitow!!"
+	var plainText string
+	fmt.Print("Enter plaintext to cipher (must be 8 characters long [64-bits]): ")
+	fmt.Scan(&plainText)
 
-	cipher := des.Cipher3DES(plainText, keys);
-	result := des.Decipher3DES(cipher, keys);
-	fmt.Println("PLAINTEXT: ", plainText);
-	fmt.Println("Triple DES Cipher text: ", cipher);
-	fmt.Println("Triple DES Descipher result: ", result);
+	cipher := des.Cipher3DES(plainText, keys)
+	result := des.Decipher3DES(cipher, keys)
+	fmt.Println("\nPLAINTEXT: ", plainText)
+	fmt.Println("Triple DES Cipher text: ", cipher)
+	fmt.Println("Triple DES Descipher result: ", result)
 }
-
